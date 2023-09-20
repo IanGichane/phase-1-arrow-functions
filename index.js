098f572d9a589dafce749c0ae625fe8755beb4f7
Ian <gichaneian@gmail.com>
2023-09-20 15:05:40 +0300

    Completed assignment with one error

---
+++ index.js
@@ -0,0 +1,10 @@
+//divide
+const divide = (a, b) => a/b;
+let result = divide(2000,100)
+console.log(result)
+//square
+const square = (c)=> c**2;
+square(2)
+//add
+const add = (e,f)=> e+f;
+add(200,100);
